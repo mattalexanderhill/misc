edac39e5c9ea3f91a65bf7f69981595ab34f4613
Matt Hill <matt@wearisma.com>
2019-12-23 14:54:25 +0000

    Finished work on poker

--- src/poker.rs
+++ src/poker.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 use std::cmp::Ordering;
 use std::ops::Index;
 
@@ -170,17 +172,71 @@ impl Hand {
         }
     }
 
-    fn get_category(&self) -> Category {
-        if self.is_full_house() { return Category::FullHouse }
-        if self.is_straight_flush() { return Category::StraightFlush }
-        if self.is_x_of_a_kind(4) { return Category::FourOfAKind }
-        if self.is_full_house() { return Category::FourOfAKind }
-        if self.is_flush() { return Category::Flush }
-        if self.is_straight() { return Category::Straight }
-        if self.is_x_of_a_kind(3) { return Category::ThreeOfAKind }
-        if self.is_two_pair() { return  Category::TwoPairs }
-        if self.is_x_of_a_kind(2) { return  Category::OnePair }
-        Category::HighCard
+    fn high_rank(&self) -> Rank {
+        let mut highest: Rank = self[0].rank;
+
+        for i in 1..5 {
+            if self[i].rank > highest {
+                highest = self[i].rank;
+            }
+        }
+
+        highest
+    }
+
+    fn cmp(&self, other: Self) -> Ordering {
+      let (score, rank) = self.score();
+      let (score_other, rank_other) = other.score();
+
+      if score > score_other { return Ordering::Greater; } 
+      if score < score_other { return Ordering::Less; } 
+
+      if rank > rank_other { return Ordering::Greater; } 
+      if rank < rank_other { return Ordering::Less; } 
+
+      let mut ranks = self.ranks();
+      let mut other_ranks = other.ranks();
+      ranks.sort();
+      other_ranks.sort();
+
+      for i in (0..5).rev() {
+        if ranks[i] > other_ranks[i] { return Ordering::Greater}
+        if ranks[i] < other_ranks[i] { return Ordering::Less}
+      }
+
+      Ordering::Equal
+    } 
+
+    fn score(&self) -> (Category, Rank) {
+        if self.is_royal_flush() {
+            return (Category::RoyalFlush, Rank::Ace);
+        }
+        if let Some(r) = self.straight_flush() {
+            return (Category::StraightFlush, r);
+        }
+        if let Some(r) = self.x_of_a_kind(4) {
+            return (Category::FourOfAKind, r)
+        }
+        if let Some(r) = self.full_house() {
+            return (Category::FullHouse, r);
+        }
+        if let Some(r) = self.flush() {
+            return (Category::Flush, r);
+        }
+        if let Some(r) = self.straight() {
+            return (Category::Straight, r);
+        }
+        if let Some(r) = self.x_of_a_kind(3) {
+            return (Category::ThreeOfAKind, r);
+        }
+        if let Some(r) = self.two_pair() {
+            return (Category::TwoPairs, r);
+        }
+        if let Some(r) = self.x_of_a_kind(2) {
+            return (Category::OnePair, r);
+        }
+
+        return (Category::HighCard, self.high_rank());
     }
 
     fn ranks(&self) -> Vec<&Rank>{
@@ -223,24 +279,38 @@ impl Hand {
         counts
     }
 
-    fn is_x_of_a_kind(&self, x: u8) -> bool {
+    fn x_of_a_kind(&self, x: u8) -> Option<Rank> {
+        let mut rank: Option<Rank> = None;
+
         for i in 0..5 {
             let mut counter = 0;
 
             for j in i..5 {
                 if &self[j].rank == &self[i].rank {
                     counter += 1;
-                }
-                if counter >= x {
-                    return true;
+                    if counter >= x {
+                        match rank {
+                            None => {
+                                rank = Some(self[i].rank.clone());
+                            },
+                            Some(r) if r < self[i].rank => {
+                                rank = Some(self[i].rank.clone());
+                            },
+                            _ => (),
+                        }
+                    }
                 }
             }
         }
-        false
+        rank
     }
 
-    fn is_straight(&self) -> bool {
-        let mut lowest = &self.one.rank;
+    fn is_x_of_a_kind(&self, x: u8) -> bool {
+        self.x_of_a_kind(x).is_some()
+    }
+
+    fn is_straight(&self) -> bool{
+        let mut lowest = &self[0].rank;
         for i in 1..5 {
             if &self[i].rank < lowest {
                 lowest = &self[i].rank;
@@ -262,43 +332,97 @@ impl Hand {
         true
     }
 
-    fn is_flush(&self) -> bool {
+    fn straight(&self) -> Option<Rank> {
+        if self.is_straight() {
+            Some(self.high_rank())
+        } else {
+            None
+        }
+    }
+
+    fn flush(&self) -> Option<Rank> {
         for i in 0..5 {
             if &self[i].suit != &self[0].suit {
-                return false;
+                return None;
             }
         }
-        true
+        Some(self.high_rank())
+    }
+
+    fn is_flush(&self) -> bool {
+        self.flush().is_some()
+    }
+
+    fn two_pair(&self) -> Option<Rank> {
+        match self.rank_counts().as_slice() {
+            [1, 2, 2] | [2, 1, 2] | [2, 2, 1] => {
+                self.x_of_a_kind(2)
+            },
+            _ => None
+        }
     }
 
     fn is_two_pair(&self) -> bool {
-        match self.rank_counts().as_slice() {
-            [1, 2, 2] => true,
-            [2, 1, 2] => true,
-            [2, 2, 1] => true,
-            _ => false
+        self.two_pair().is_some()
+    }
+
+    fn straight_flush(&self) -> Option<Rank> {
+        if self.is_flush() && self.is_straight() {
+            Some(self.high_rank())
+        } else {
+            None
         }
     }
 
     fn is_straight_flush(&self) -> bool {
-        self.is_flush() && self.is_straight()
+        self.straight_flush().is_some()
     }
 
     fn is_royal_flush(&self) -> bool {
-        self.contains_rank(&Rank::Ace) && self.is_straight_flush()
-    }
-
-    fn full_house(&self) -> bool {
-        match self.rank_counts().as_slice() {
-            [3, 2] => true,
-            [2, 3] => true,
+        match self.straight_flush() {
+            Some(Rank::Ace) => true,
             _ => false
         }
     }
+
+    fn full_house(&self) -> Option<Rank> {
+        match self.rank_counts().as_slice() {
+            [3, 2] | [2, 3] => self.x_of_a_kind(3),
+            _ => None
+        }
+    }
+
+    fn is_full_house(&self) -> bool {
+        self.full_house().is_some()
+    }
+}
+
+fn problem() -> std::io::Result<(u32, u32, u32)> {
+    let f = File::open("resources/poker.txt")?;
+    let reader = BufReader::new(f);
+
+    let mut wins_one = 0;
+    let mut wins_two = 0;
+    let mut draws = 0;
+
+    for line in reader.lines() {
+      let line = line.unwrap();
+      let (one, two) = line.split_at(14);
+      let hand_one = Hand::from_str(&one).unwrap();
+      let hand_two = Hand::from_str(&two).unwrap();
+
+      match hand_one.cmp(hand_two) {
+        Ordering::Greater => wins_one += 1,
+        Ordering::Less    => wins_two += 1,
+        Ordering::Equal   => draws += 1,
+      }
+    }
+
+    Ok((wins_one, wins_two, draws))
 }
 
 #[cfg(test)]
-mod tests {
+mod poker_tests {
     use super::*;
 
     #[test]
@@ -335,7 +459,7 @@ mod tests {
     }
 
     #[test]
-    fn test_is_x_of_a_kind() {
+    fn test_x_of_a_kind() {
         let hand = Hand {
             zero:  Card{rank: Rank::One,   suit: Suit::Hearts},
             one:   Card{rank: Rank::Two,   suit: Suit::Hearts},
@@ -344,8 +468,10 @@ mod tests {
             four:  Card{rank: Rank::Two,   suit: Suit::Hearts},
         };
 
-        assert!(hand.is_x_of_a_kind(3));
-        assert!(!hand.is_x_of_a_kind(4));
+        assert_eq!(hand.is_x_of_a_kind(3), true);
+        assert_eq!(hand.x_of_a_kind(3), Some(Rank::Two));
+        assert_eq!(hand.is_x_of_a_kind(4), false);
+        assert_eq!(hand.x_of_a_kind(4), None);
     }
 
     #[test]
@@ -407,10 +533,10 @@ mod tests {
         assert_eq!(a.rank_counts(), vec![2, 2, 1]);
 
         let b = Hand {
-            zero:  Card{rank: Rank::One,  suit: Suit::Hearts},
-            one:   Card{rank: Rank::Two,  suit: Suit::Hearts},
+            zero:  Card{rank: Rank::One,   suit: Suit::Hearts},
+            one:   Card{rank: Rank::Two,   suit: Suit::Hearts},
             two:   Card{rank: Rank::Three, suit: Suit::Hearts},
-            three: Card{rank: Rank::Four, suit: Suit::Hearts},
+            three: Card{rank: Rank::Four,  suit: Suit::Hearts},
             four:  Card{rank: Rank::One,   suit: Suit::Clubs},
         };
 
@@ -428,16 +554,16 @@ mod tests {
     }
 
     #[test]
-    fn test_is_two_pair() {
+    fn test_two_pair() {
         let a = Hand {
             zero:  Card{rank: Rank::One,   suit: Suit::Hearts},
             one:   Card{rank: Rank::Two,   suit: Suit::Hearts},
-            two:   Card{rank: Rank::Three,   suit: Suit::Hearts},
+            two:   Card{rank: Rank::Three, suit: Suit::Hearts},
             three: Card{rank: Rank::Two,   suit: Suit::Hearts},
-            four:  Card{rank: Rank::One, suit: Suit::Hearts},
+            four:  Card{rank: Rank::One,   suit: Suit::Hearts},
         };
 
-        assert!(a.is_two_pair());
+        assert_eq!(a.two_pair(), Some(Rank::Two));
 
         let b = Hand {
             zero:  Card{rank: Rank::One,   suit: Suit::Hearts},
@@ -447,7 +573,7 @@ mod tests {
             four:  Card{rank: Rank::Three, suit: Suit::Hearts},
         };
 
-        assert!(!b.is_two_pair());
+        assert_eq!(b.two_pair(), None);
     }
 
     #[test]
@@ -471,5 +597,58 @@ mod tests {
         };
 
         assert!(!b.is_full_house());
+    }
+
+    #[test]
+    fn test_score() {
+        let a = Hand::from_str("8C 8S KC 9H 9S").unwrap();
+        let b = Hand::from_str("7D 2S 5D 3S AC").unwrap();
+        let c = Hand::from_str("5C AC 5C AC 9C").unwrap();
+
+        assert_eq!(a.score(), (Category::TwoPairs, Rank::Nine));
+        assert_eq!(b.score(), (Category::HighCard, Rank::Ace));
+        assert_eq!(c.score(), (Category::Flush, Rank::Ace));
+    }
+
+    #[test]
+    fn test_cmp() {
+        let a = Hand::from_str("5H 5C 6S 7S KD").unwrap();
+        let b = Hand::from_str("2C 3S 8S 8D TD").unwrap();
+
+        assert_eq!(a.cmp(b), Ordering::Less);
+
+        let a = Hand::from_str("5D 8C 9S JS AC").unwrap();
+        let b = Hand::from_str("2C 5C 7D 8S QH").unwrap();
+
+        assert_eq!(a.cmp(b), Ordering::Greater);
+
+        let a = Hand::from_str("2D 9C AS AH AC").unwrap();
+        let b = Hand::from_str("3D 6D 7D TD QD").unwrap();
+
+        assert_eq!(a.cmp(b), Ordering::Less);
+
+        let a = Hand::from_str("4D 6S 9H QH QC").unwrap();
+        let b = Hand::from_str("3D 6D 7H QD QS").unwrap();
+
+        assert_eq!(a.cmp(b), Ordering::Greater);
+
+        let a = Hand::from_str("2H 2D 4C 4D 4S").unwrap();
+        let b = Hand::from_str("3C 3D 3S 9S 9D").unwrap();
+
+        assert_eq!(a.cmp(b), Ordering::Greater);
+
+        let a = Hand::from_str("6D 7C 5D 5H 3S").unwrap();
+        let b = Hand::from_str("5C JC 2H 5S 3D").unwrap();
+
+        assert_eq!(a.cmp(b), Ordering::Less);
+    }
+
+    #[test]
+    fn test_problem() {
+      let (wins_one, wins_two, draws) = problem().unwrap();
+
+      assert_eq!(wins_one, 376);
+      assert_eq!(wins_two, 624);
+      assert_eq!(draws,    0);
     }
 }
